6ae9bf9d4ad5732b341773c6f7d16ad289c3779e
game301 <hurkaparty@gmail.com>
2023-10-04 17:00:12 +0200

    changed elements with"cursor pointer" to buttons

--- src/components/NavIconContainer.jsx
+++ src/components/NavIconContainer.jsx
@@ -8,24 +8,24 @@ const NavIconContainer = () => {
     }
 
     return (
-        <ul
+        <div
             id='nav-icon-container'
             className='flex justify-end items-center dark:text-darkModePrimaryColor ml-auto text-navIconContainerFontSize'>
-            <li className='ps-2 pe-2 cursor-pointer'>
+            <button className='ps-2 pe-2'>
                 <i id='nav-search' className='fa-solid fa-magnifying-glass'></i>
-            </li>
-            <li className='ps-2 pe-2 cursor-pointer'>
+            </button>
+            <button className='ps-2 pe-2'>
                 <i
                     id='nav-dark-mode-toggle'
                     className={`fa-solid w-navDarkModeToggleWidth fa ${
                         isDarkMode ? "fa-moon" : "fa-sun"
                     }`}
                     onClick={handleDarkModeToggle}></i>
-            </li>
-            <li className='ps-2 pe-2 mr-2 cursor-pointer'>
+            </button>
+            <button className='ps-2 pe-2 mr-2'>
                 <i id='nav-login' className='fa-solid fa-key'></i>
-            </li>
-        </ul>
+            </button>
+        </div>
     )
 }
 
