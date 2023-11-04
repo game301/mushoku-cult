b11bc36e9a6fe1e0e7b87d81838b18dcd1a28632
game301 <hurkaparty@gmail.com>
2023-11-04 16:23:03 +0100

    deleted garbage

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
-import dotenv from "dotenv"
-dotenv.config()
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    define: {
-        "process.env.VITE_AMOGUS": JSON.stringify(process.env.VITE_AMOGUS),
-    },
 })
