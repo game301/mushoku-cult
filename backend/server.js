21ac704f4b2142e54658e0650ffea11ca00443e8
game301 <hurkaparty@gmail.com>
2023-11-07 01:56:56 +0100

    deleted envKeys.js, added .env variables

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,12 @@
-//import dotenv from "dotenv"
-//dotenv.config()
+import dotenv from "dotenv"
+dotenv.config()
 //console.log(dotenv.config())
-
-import envKeys from "./envKeys.js"
+//console.log(process.env)
 
 import express, { json } from "express"
 import mongoose from "mongoose"
 
 import workoutRoutes from "./routes/workouts.js"
-
-// deleteable
-/*
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-//console.log(__dirname)
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") })
-//console.log(dotenv.config({ path: path.resolve(__dirname, "../.env") }))
-//console.log(dotenv.config({ path: __dirname + "/../.env" }))
-*/
-
-//console.log(import.meta)
-//console.log(import.meta.url)
-//console.log(import.meta.env)
-//console.log(import.meta.env.VITE_PORT)
 
 // express app
 const app = express()
@@ -44,18 +24,12 @@ app.use("/api/workouts", workoutRoutes)
 
 // connect to db
 mongoose
-    //.connect(import.meta.env.MONGO_URI)
-    .connect(envKeys.mongoURI)
+    .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("connected to database")
         // listen to port
-        //app.listen(import.meta.env.VITE_PORT, () => {
-        app.listen(envKeys.port, () => {
-            console.log(
-                "listening for requests on port",
-                //import.meta.env.VITE_PORT
-                envKeys.port
-            )
+        app.listen(process.env.PORT, () => {
+            console.log("listening for requests on port", process.env.PORT)
         })
     })
     .catch((err) => {
